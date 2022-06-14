d69a9eaa2966d6ee8b6df66bcc1d5060f856679e
Marie <Marue59@hotmail.fr>
2022-06-14 14:26:56 +0200

    first commit
    initialisation de projet

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,21 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavBarComponent } from './_commons/nav-bar/nav-bar.component';
+import { NotFoundComponent } from './_commons/not-found/not-found.component';
+import { ProductPageComponent } from './pages/product-page/product-page.component';
+import { HomePageComponent } from './pages/home-page/home-page.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavBarComponent,
+    NotFoundComponent,
+    ProductPageComponent,
+    HomePageComponent,
   ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+  imports: [BrowserModule, AppRoutingModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
