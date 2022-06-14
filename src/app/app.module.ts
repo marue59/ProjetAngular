2d3050d7469b073657aba81a03f74c5f95c553ae
Marie <Marue59@hotmail.fr>
2022-06-14 15:55:43 +0200

    feat: front de la home :tada:

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavBarComponent } from './_commons/nav-bar/nav-bar.component';
+import { FooterComponent } from './_commons/footer/footer.component';
 import { NotFoundComponent } from './_commons/not-found/not-found.component';
 import { ProductPageComponent } from './pages/product-page/product-page.component';
 import { HomePageComponent } from './pages/home-page/home-page.component';
@@ -12,6 +13,7 @@ import { HomePageComponent } from './pages/home-page/home-page.component';
   declarations: [
     AppComponent,
     NavBarComponent,
+    FooterComponent,
     NotFoundComponent,
     ProductPageComponent,
     HomePageComponent,
