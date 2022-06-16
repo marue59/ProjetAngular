2089a8eafe081d4b326241e4d759b05ba3fddd4f
Marie <Marue59@hotmail.fr>
2022-06-16 11:11:42 +0200

    feat: ajout des souscription de la page detail :boom:

--- src/app/pages/detail-page/detail-page.component.ts
+++ src/app/pages/detail-page/detail-page.component.ts
@@ -1,6 +1,15 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, RouterOutlet } from '@angular/router';
-import { ProductPageComponent } from '../product-page/product-page.component';
+
+// import { ProductPageComponent } from '../product-page/product-page.component';
+
+// import des Models
+import Album from 'src/app/models/album.model';
+import Film from 'src/app/models/film.model';
+
+// import des Services
+import { AlbumService } from 'src/app/services/album.service';
+import { FilmService } from 'src/app/services/film.service';
 
 @Component({
   selector: 'app-detail-page',
@@ -8,21 +17,56 @@ import { ProductPageComponent } from '../product-page/product-page.component';
   styleUrls: ['./detail-page.component.scss'],
 })
 export class DetailPageComponent implements OnInit {
+  /*
   // a ne pas reproduire mais peut dépanner car nous n'avons pas de services
   films = new ProductPageComponent().films;
   albums = new ProductPageComponent().albums;
 
   products = this.films.concat(this.albums);
+*/
 
-  product!: any;
+  //type soit films soit albums
+  product!: Film | Album;
 
-  constructor(private routeur: RouterOutlet, private route: ActivatedRoute) {}
-
+  constructor(
+    private routeur: RouterOutlet,
+    private route: ActivatedRoute,
+    private albumService: AlbumService,
+    private filmService: FilmService
+  ) {}
+  /*
   ngOnInit(): void {
     // je vais chercher this.route et un objet qui contient snapshot et paramMap...
     const id = this.route.snapshot.paramMap.get('id');
     if (id) {
       this.product = this.products.find((product) => product.id == id);
+    }
+  }
+*/
+  ngOnInit(): void {
+    const id = this.route.snapshot.paramMap.get('id');
+  }
+
+  private suscribeFilm(id: number): void {
+    this.filmService.getFilm(id).subscribe((film: Film) => {
+      this.product = film;
+    });
+  }
+
+  private suscribeAlbum(id: number): void {
+    this.albumService.getAlbum(id).subscribe((album: Album) => {
+      this.product = album;
+    });
+  }
+
+  setSuscribe(type: string | null, id: string | null) {
+    if (id && type === 'albums') {
+      // rajouter le +id permet de caster la chaine de caractere en nombre
+      this.suscribeAlbum(+id);
+    } else if (id && type === 'films') {
+      this.suscribeFilm(+id);
+    } else if (!id || !type) {
+      this.routeur.navigate(['/not-found']);
     }
   }
 }
--- src/app/pages/product-page/product-page.component.ts
+++ src/app/pages/product-page/product-page.component.ts
@@ -1,12 +1,36 @@
 import { Component, OnInit } from '@angular/core';
 
+// import des Models
+import Album from 'src/app/models/album.model';
+import Film from 'src/app/models/film.model';
+
+// import des Services
+import { AlbumService } from 'src/app/services/album.service';
+import { FilmService } from 'src/app/services/film.service';
+
 @Component({
   selector: 'app-product-page',
   templateUrl: './product-page.component.html',
   styleUrls: ['./product-page.component.scss'],
 })
 export class ProductPageComponent implements OnInit {
-  constructor() {}
+  films: Film[] = [];
+  albums: Album[] = [];
 
-  ngOnInit(): void {}
+  constructor(
+    private filmService: FilmService,
+    private albumService: AlbumService
+  ) {}
+
+  ngOnInit(): void {
+    // recuperer la liste de tout les films
+    // utiliser l'observable qui est un objet et qui du coup a des methodes
+    // faire fonction anonyme
+    this.filmService.getFilms().subscribe((films) => {
+      this.films = films;
+    });
+    this.albumService.getAlbums().subscribe((albums) => {
+      this.albums = albums;
+    });
+  }
 }
